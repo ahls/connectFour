87d96464ed03a1346ab14d2038d91ee543dc2c90
ahls <ahlon.sin@gmail.com>
2023-01-31 23:47:52 -0800

    fixed some comments

--- connect4.js
+++ connect4.js
@@ -140,25 +140,24 @@ function CheckForTie()
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
-  function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
-    return cells.every(
+  // Check four cells to see if they're all color of current player
+  //  - cells: list of four (y, x) cells
+  //  - returns true if all are legal coordinates & all match currPlayer
+  const _win = (cells)=>
+    cells.every(
       ([y, x]) =>
         y >= 0 &&
         y < HEIGHT &&
         x >= 0 &&
-        x < WIDTH &&
+        x < WIDTH && //from top to bottom, terminate early if the x or y is out of bounds
         board[y][x] === currPlayer
     );
-  }
+  
 
   // TODO: read and understand this code. Add comments to help you.
 
   for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {//look for each andevery cell and see if there is any case where the below condition is met.
+    for (let x = 0; x < WIDTH; x++) { // assigning all 4 possible combinations from each and every cell.
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       let vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       let diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
